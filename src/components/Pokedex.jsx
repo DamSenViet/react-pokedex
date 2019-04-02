5a8e4591fb52d747259db637be53c4dbca1316a6
Viet Tran <vietdaitran1998@gmail.com>
2019-04-01 20:59:20 -0700

    Basic setup finished

---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './../css/Pokedex.css';
+
+class Pokedex extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+	}
+
+	render() {
+		return (
+			<div className="pokedex">
+			
+			</div>
+		);
+	}
+}
+
+export default Pokedex;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/reset.css' // need to reset the body for cross-browser consistency
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Pokedex from './components/Pokedex';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Pokedex />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
